d1069eef59bba647e87eeff5c7e411ed3cdb162e
Nikos Filinis <nfilinis@netmode.ntua.gr>
2024-04-01 10:40:39 +0000

    Update database models

--- src/models/graph.py
+++ src/models/graph.py
@@ -7,15 +7,11 @@ from sqlalchemy.dialects.postgresql import JSONB
 class Graph(db.Model):
     __tablename__ = 'graph'
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-    name = db.Column(
-        db.String(255),
-        unique=True,
-        nullable=False
-    )
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    status = db.Column(db.String(255))
+    project = db.Column(db.String(255))
+    grafana = db.Column(db.String(255))
     graph_descriptor = db.Column(JSONB)
 
     services = db.relationship(
@@ -23,3 +19,20 @@ class Graph(db.Model):
         back_populates='graph',
         cascade='all,delete'
     )
+
+    def to_dict(self):
+        """Returns a dictionary representation of the class."""
+
+        instance_dict = {
+            'name': self.name,
+            'status': self.status,
+            'project': self.project,
+            'grafana': self.grafana,
+            'descriptor': self.graph_descriptor,
+        }
+
+        instance_dict['services'] = [
+            service.to_dict() for service in self.services
+        ]
+
+        return instance_dict
--- src/models/service.py
+++ src/models/service.py
@@ -7,15 +7,14 @@ from sqlalchemy.dialects.postgresql import JSONB
 class Service(db.Model):
     __tablename__ = 'service'
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-    name = db.Column(
-        db.String(255),
-        unique=True,
-        nullable=False
-    )
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    status = db.Column(db.String(255))
+    grafana = db.Column(db.String(255))
+    cluster_affinity = db.Column(db.String(255))
+    artifact_ref = db.Column(db.String(255))
+    artifact_type = db.Column(db.String(255))
+    resources = db.Column(JSONB)
     values_overwrite = db.Column(JSONB)
 
     graph = db.relationship('Graph', back_populates='services')
@@ -24,3 +23,19 @@ class Service(db.Model):
         db.ForeignKey('graph.id'),
         nullable=False
     )
+
+    def to_dict(self):
+        """Returns a dictionary representation of the class."""
+
+        instance_dict = {
+            'name': self.name,
+            'status': self.status,
+            'grafana': self.grafana,
+            'cluster_affinity': self.cluster_affinity,
+            'resources': self.resources,
+            'values_overwrite': self.values_overwrite,
+            'artifact_ref': self.artifact_ref,
+            'artifact_type': self.artifact_type
+        }
+
+        return instance_dict
